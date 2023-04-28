362e8558646faac453b9d6b6484086df0fcb8e62
Mohamed Fayyad <mohamedfayad080@gmail.com>
2023-04-29 01:17:26 +0300

    Add project files.

---
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization;
+using TestCoreApp.Models;
+
+namespace TestCoreApp.Controllers
+{
+	public class ItemsController : Controller
+	{
+		private readonly AppDbContext _db;
+
+		public ItemsController(AppDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			IEnumerable<Item> itemsList = _db.Items.ToList();
+
+			return View(itemsList);
+		}
+
+        //Get
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Item item)
+        {
+
+            if (ModelState.IsValid) // this statement for server_side_validatoin
+            {
+                _db.Items.Add(item);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(item);
+            }
+        }
+
+		//Get
+		public IActionResult Edit(int? Id)
+		{
+			if (Id == null || Id ==0) //Check if the ID exists or not
+            {
+				return NotFound();
+			}
+
+			var item = _db.Items.Find(Id); // if Id exists then return item
+			if (item == null)
+			{
+				return NotFound();
+			}
+			return View(item); // sent item to Edit view
+		}
+
+		//Post
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Item item)
+		{
+			if (ModelState.IsValid) // this for server_side_validatoin
+			{
+                _db.Items.Update(item);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			else
+			{
+				return View(item);
+			}
+		}
+
+		//Get
+		public IActionResult Delete(int? Id)
+		{
+			if (Id == null || Id ==0) //Check if the ID exists or not
+            {
+				return NotFound();
+			}
+
+			var item = _db.Items.Find(Id); // if Id exists then return item
+			if (item == null)
+			{
+				return NotFound();
+			}
+			return View(item); // sent item to Delete view
+		}
+
+		//Post
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(Item item)
+		{
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+		}
+    }
+}
---
+++ TestCoreApp/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestCoreApp.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+		{
+
+		}
+		public DbSet<Item> Items { get; set; }
+
+	}
+}
